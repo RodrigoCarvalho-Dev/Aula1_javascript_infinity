d39e16678b1d375c3779d4362fed3eb97124e168
Rodrigo Carvalho Dev <rodrigosms103042r6@gmail.com>
2024-02-08 21:29:43 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,17 @@
+"use strict";
+const name_person = prompt("Digite seu nome:");
+console.log(name_person);
+const hall = {
+    nome: "Rodrigo",
+    age: 18,
+    past: "/src/index.js"
+};
+console.log(hall.past);
+let age = 18;
+if (age = 18) {
+    console.log("hello");
+}
+const number_person = Number(prompt("Digite seu número: "));
+const number_person2 = Number(prompt("Digite seu 2º número: "));
+const sum_number = number_person + number_person2;
+console.log(`${number_person} is the first number and ${number_person2} is the 2º number  and the sum is ${sum_number}`);
